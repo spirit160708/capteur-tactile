ef56605fed84f4e79120dc64688db7f7339a0fc6
spirit160708 <103181350+spirit160708@users.noreply.github.com>
2022-04-21 11:18:34 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Surprised)
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Square)
+})
